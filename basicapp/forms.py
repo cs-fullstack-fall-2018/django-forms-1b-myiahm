646e84d4eb1ec0ae9e183cdb4ad7e830b8fae12a
Myiah Moore <myiahsade@gmail.com>
2018-10-05 14:58:35 -0500

    check in

--- basicapp/forms.py
+++ basicapp/forms.py
@@ -5,3 +5,6 @@ class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
+
+class WebsiteForm(forms.Form):
+    entry = forms.CharField()
